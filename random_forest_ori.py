3c36d1629a9c077ec8fc737f5e32c62f4385df95
fadilparves <fadil.parves@gmail.com>
2020-09-10 18:24:42 +0800

    working on ori model first

--- random_forest_ori.py
+++ random_forest_ori.py
@@ -10,13 +10,6 @@ from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from cuml.ensemble import RandomForestRegressor as curfc
 import cudf as cd
 
-import ray
-from ray import tune
-from ray.tune import track, trial
-from ray.tune.logger import TBXLogger
-from ray.tune.schedulers import AsyncHyperBandScheduler, MedianStoppingRule
-from ray.tune.utils import get_pinned_object, pin_in_object_store
-
 start_time = time.time()
 
 data = cd.read_csv("data.csv")
@@ -29,5 +22,5 @@ data.drop(['﻿', 'ID', 'Name', 'Age', 'Photo', 'Nationality', 'Flag',
            'LF', 'CF', 'RF', 'RW', 'LAM', 'CAM', 'RAM', 'LM', 'LCM', 'CM', 'RCM',
            'RM', 'LWB', 'LDM', 'CDM', 'RDM', 'RWB', 'LB', 'LCB', 'CB', 'RCB', 'RB',], inplace=True)
 
-print(data.columns)
+print(data.head(10))
 print(data.info(verbose=True))
