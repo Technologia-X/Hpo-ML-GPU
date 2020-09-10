3c2a27b3c1c7f24c415b8f112a9cf3b4c0ad815f
fadilparves <fadil.parves@gmail.com>
2020-09-10 19:17:55 +0800

    select scheduling algorithm method done

--- random_forest_hpo.py
+++ random_forest_hpo.py
@@ -217,3 +217,39 @@ def build_search_alg(search_alg, param_ranges: dict):
     else:
         print("Unknown Option. Select BayesOpt or SkOpt")
     return alg
+
+def select_sched_alg(sched_alg):
+    """
+     Initialize a scheduling algorithm that is selected using 'sched_alg'
+    
+    Parameters
+    ----------
+        sched_alg   : str; Selecting the search algorithm. Possible values
+                       [MedianStop, AsyncHyperBand]
+
+    Returns
+    -------
+        alg : Object of the RayTune scheduling algorithm selected
+    """
+    sched = None
+    if sched_alg == "AsyncHyperBand":
+        sched = AsyncHyperBandScheduler(
+            time_attr="training_iteration",
+            metric="test_accuracy",
+            mode="max",
+            max_t=50,
+            grace_period=1,
+            reduction_factor=3,
+            brackets=3,
+        )
+    elif sched_alg == "MedianStop":
+        sched = MedianStoppingRule(
+            time_attr="time_total_s",
+            metric="test_accuracy",
+            mode="max",
+            grace_period=1,
+            min_samples_required=3,
+        )
+    else:
+        print("Unknown Option. Select MedianStop or AsyncHyperBand")
+    return sched
